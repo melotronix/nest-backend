e536003f94334bc50638c81baaf4f4d430d34f68
andelight <andeligh.99@gmail.com>
2022-04-02 15:36:14 +0300

    ✨ Creating an auth-guard

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
 import { SequelizeModule } from '@nestjs/sequelize';
@@ -6,11 +6,18 @@ import { User } from './models/users.model';
 import { Role } from '../roles/models/roles.model';
 import { UserRoles } from '../roles/models/user-roles.model';
 import { RolesModule } from '../roles/roles.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [SequelizeModule.forFeature([User, Role, UserRoles]), RolesModule],
-  exports: [UsersService]
+  imports: [
+    SequelizeModule.forFeature([User, Role, UserRoles]),
+    RolesModule,
+    forwardRef(() => AuthModule)
+  ],
+  exports: [
+    UsersService
+  ]
 })
 export class UsersModule {}
